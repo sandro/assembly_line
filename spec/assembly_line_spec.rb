0e631c9d8255933183a113e557d2afd848d23e0d
Sandro Turriate <sandro.turriate@gmail.com>
2010-02-03 18:16:57 -0500

    A few specs

--- spec/assembly_line_spec.rb
+++ spec/assembly_line_spec.rb
@@ -1,7 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "AssemblyLine" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe AssemblyLine do
+  context "API" do
+    subject { AssemblyLine }
+    it { should respond_to(:define) }
+    it { should respond_to(:assemble) }
+
+  end
+
+  describe ".define" do
+    it "adds the AssemblyLine to the Registry" do
+      definition = lambda {}
+      AssemblyLine::Registry.should_receive(:add).with(:user_with_address, definition)
+      AssemblyLine.define(:user_with_address, &definition)
+    end
   end
 end
