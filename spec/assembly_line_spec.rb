073395b692ca742c3b6b71bd20a1e9dce0de9735
Sandro Turriate <sandro.turriate@gmail.com>
2010-02-03 19:48:56 -0500

    AssemblyLine specs

--- spec/assembly_line_spec.rb
+++ spec/assembly_line_spec.rb
@@ -15,4 +15,35 @@ describe AssemblyLine do
       AssemblyLine.define(:user_with_address, &definition)
     end
   end
+
+  describe ".assemble" do
+    let(:rspec_context) { mock('rspec context') }
+
+    context "cannot locate the AssemblyLine" do
+      it "raises an error" do
+        expect do
+          AssemblyLine.assemble(:does_not_exist, rspec_context)
+        end.to raise_error(ArgumentError)
+      end
+    end
+
+    context "AssemblyLine exists" do
+      it "assembles the AssemblyLine" do
+        constructor = mock('constructor')
+        constructor.should_receive(:assemble).with(rspec_context, options)
+        AssemblyLine::Registry.stub(:locate => constructor)
+        AssemblyLine.assemble(:party, rspec_context)
+      end
+    end
+  end
+
+  describe "#Assemble" do
+    it "delegates to AssemblyLine.assemble" do
+      klass = Class.new do
+        extend AssemblyLine
+      end
+      AssemblyLine.should_receive(:assemble).with(:party, klass, {})
+      klass.Assemble(:party)
+    end
+  end
 end
